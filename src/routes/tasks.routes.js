729d8f3600537ace130a5316a8882961ce75734c
sr-roboto <marcoslopez123.lu@gmail.com>
2024-08-09 15:52:29 -0300

    cree las validaciones para el create, update, delete

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 const router = express.Router();
 import { obtenerTarea, obtenerTareas, actualizarTarea, eliminarTarea, crearTareas } from '../controllers/tasks.controllers.js'
-import { applyValidations, validacionesDeTaskCreate } from '../validations.js';
+import { applyValidations, validacionesDeTaskCreate, validacionesDeTaskDelete, validacionesDeTaskUpdate } from '../validations/validations.js';
 
 router.post('/tasks', validacionesDeTaskCreate , applyValidations , crearTareas);
 router.get('/tasks', obtenerTareas);
 router.get('/task/:id', obtenerTarea);
-router.put('/task/:id', actualizarTarea);
-router.delete('/task/:id', eliminarTarea);
+router.put('/task/:id', validacionesDeTaskUpdate, applyValidations, actualizarTarea);
+router.delete('/task/:id', validacionesDeTaskDelete, applyValidations, eliminarTarea);
 
 export { router };
--- src/validations/validations.js
+++ src/validations/validations.js
@@ -3,14 +3,16 @@ import { body, param, validationResult } from "express-validator";
 export const validacionesDeTaskCreate = [
     body('title')
     .isString().withMessage("el titulo debe de ser una cadena de texto")
-    .notEmpty().withMessage("el titulo no puede estar vacio"),
+    .notEmpty().withMessage("el titulo no puede estar vacio")
+    .isLength({
+        min: 6,
+        max: 21
+    }).withMessage('el title debe ser mayor que 5 y menor que 20'),
     body('description')
     .isString().withMessage("la descripcion debe de ser una cadena de texto")
     .notEmpty().withMessage("la descripcion no puede estar vacia"),
     body('isComplete')
-    .optional()
     .isBoolean().withMessage('este campo solo acepta valores booleanos ')
-    .notEmpty().withMessage("el titulo no puede estar vacio")
 ];
 
 export const validacionesDeTaskUpdate = [
@@ -28,13 +30,18 @@ export const validacionesDeTaskUpdate = [
     .notEmpty().withMessage("el titulo no puede estar vacio")
 ];
 
+export const validacionesDeTaskDelete = [
+    param('id')
+    .isNumeric().withMessage('el parametro tiene que ser numerico')
+]
+
+
+//aplicamos las validaciones
 export const applyValidations = (req, res, next) => {
     const errores = validationResult(req)
     if (!errores.isEmpty()){
-        return res.status(400).json({
-            erros:errores.array()
-        })
+        const errors = errores.array().map(a => a.msg)
+        return res.status(400).json(errors)
     }
-
     next();
 };
