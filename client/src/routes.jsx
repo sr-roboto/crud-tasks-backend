0538c0b988b1bb6b2d1764f6514b42c755eee901
sr-roboto <marcoslopez123.lu@gmail.com>
2024-09-01 09:14:09 -0300

    Optimización del componente ProtectedRoute y adición de la rueda de carga

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -2,9 +2,16 @@ import { Navigate, Outlet } from 'react-router-dom';
 import useAuth from './context/useAuth';
 
 export const ProtectedRoute = () => {
-  const { isAuthenticated, loading } = useAuth();
-
-  if (loading) return <h1>Loading...</h1>;
-  if (!isAuthenticated && !loading) return <Navigate to="/login" replace />;
+  const { loading, isAuthenticated } = useAuth();
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-white"></div>
+      </div>
+    );
+  }
+  if (!loading && !isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
   return <Outlet />;
 };
